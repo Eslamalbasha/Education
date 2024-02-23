fa3968fdc24a4268633ed8008d71a8839c964548
Sala <eslamalbasha444@gmail.com>
2024-02-23 20:50:41 +0200

    Education

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// faqs
+
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("open");
+
+    const icon = faq.querySelector(".faq-icon i");
+    if (icon.className === "ul uil-plus") {
+      icon.className = "uil uil-minus";
+    } else {
+      icon.className = "uil uil-plus";
+    }
+  });
+});
